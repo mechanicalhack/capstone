eb5d7b1fe436366934626e7c4704a34224ad4f41
Rob Lloyd <rlloyd28@gmail.com>
2020-09-27 22:34:15 -0600

    debug postgres on heroku

--- models.py
+++ models.py
@@ -3,13 +3,13 @@ from sqlalchemy import Column, String, create_engine, DateTime, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "casting_agency"
+database_name = "postgresql-parallel-65214"
 database_path = "postgres://{}/{}".format('localhost:5432', database_name)
 
 db = SQLAlchemy()
 
 def setup_db(app, database_path=database_path):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql-parallel-65214'
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_path
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
